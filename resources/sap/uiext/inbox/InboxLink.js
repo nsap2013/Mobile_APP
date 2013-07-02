efbe696f9a090e3a7939d87d938e1837a63a65f6
Nsap <nader.sabra@sap.com>
2013-07-02 16:50:38 -0700

    fixed final

--- resources/sap/uiext/inbox/InboxLink.js
+++
@@ -1,6 +0,0 @@
-/*!
- * SAP UI development toolkit for HTML5 (SAPUI5)
- * 
- * (c) Copyright 2009-2013 SAP AG. All rights reserved
- */
-jQuery.sap.declare("sap.uiext.inbox.InboxLink");sap.ui.commons.Link.extend("sap.uiext.inbox.InboxLink",{init:function(){if(sap.ui.commons.Link.prototype.init){sap.ui.commons.Link.prototype.init.apply(this,arguments)}this._oBundle=sap.ui.getCore().getLibraryResourceBundle("sap.uiext.inbox")},metadata:{properties:{"overdue":{type:"boolean",defaultValue:false}}},setOverdue:function(v){this.setProperty('overdue',v);if(v){this.addStyleClass("sapUiExtInboxRedColor");this.setText("! "+this.getText())}return this},onclick:function(e){if(this.getEnabled()){if(!this.firePress({windowEvent:e})){e.preventDefault()}}else{e.preventDefault()}},setText:function(v){var V=v?v.replace(/^\s+|\s+$/g,''):v;if(!V){v=this._oBundle.getText("INBOX_TASK_TITLE_NOTAVAILABLE_MSG")}sap.ui.commons.Link.prototype.setText.call(this,[v])},setTooltip:function(v){var V=v?v.replace(/^\s+|\s+$/g,''):v;if(!V){v=this._oBundle.getText("INBOX_TASK_TITLE_NOTAVAILABLE_MSG")}else{v=this._oBundle.getText("INBOX_CURRENT_TASKTITLE_TOOLTIP")}sap.ui.commons.Link.prototype.setTooltip.call(this,v)},renderer:"sap.ui.commons.LinkRenderer"});
--- resources/sap/uiext/inbox/TCMMetadata.js
+++
@@ -1,9 +0,0 @@
-/*!
- * SAP UI development toolkit for HTML5 (SAPUI5)
- * 
- * (c) Copyright 2009-2013 SAP AG. All rights reserved
- */
-sap.ui.base.Object.extend("sap.uiext.inbox.TCMMetadata",{constructor:function(){sap.ui.base.Object.apply(this);this.oServiceMetadata=null;this.serviceSupportsFilterOption=false}});
-sap.uiext.inbox.TCMMetadata.prototype.setServiceMetadata=function(s){if(s)this.oServiceMetadata=s;this._isCustomStatusSupported()};
-sap.uiext.inbox.TCMMetadata.prototype.getServiceMetadata=function(){return this.oServiceMetadata};
-sap.uiext.inbox.TCMMetadata.prototype._isCustomStatusSupported=function(){this.serviceSupportsFilterOption=false;var t=this;var e=this.getServiceMetadata().dataServices.schema[0].entityType;var T=null;var a=null;jQuery.each(e,function(i,b){if(b.name===sap.uiext.inbox.InboxConstants.ENTITY_NAME_TASK_COLLECTION){a=b.property;jQuery.each(a,function(i,p){if(p.name===sap.uiext.inbox.InboxConstants.PROPERTY_NAME_CUSTOM_STATUS){t.serviceSupportsFilterOption=true;return false}});return false}})};
